3ec4b57e03c5dbfa0cc3f042180101d3fec34288
Suharevsky <aliksui.ua@mail.ru>
2021-01-03 12:17:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++) {
+  btnsOpenModel[i].addEventListener('click', openModal);
+}
+
+overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
